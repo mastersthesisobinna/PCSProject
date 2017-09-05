2b606f30647c86ff37f19ec805e6ac6e647f6738
mastersthesisobinna <masterthesis.obinnamogbogu@gmail.com>
2017-09-05 12:50:37 -0500

    New PCS commit

---
+++ PDFTool.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCSProject
+{
+    class PDFTool
+    {
+        private string _projectPath;
+        private string[] _pathA, _pathB;
+        private string[] _nonCoverPDFFileName, _coverPDFFileName;
+        private string _nonCoverPDFSearchPattern, _coverPDFSearchPattern;
+
+        public PDFTool(string folderName, string nonCoverPDFSearchPattern , string coverPDFSearchPattern)
+        {
+            this._projectPath = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+            this._projectPath += "\\"+ folderName;
+            this._nonCoverPDFSearchPattern = nonCoverPDFSearchPattern;  //example *_SPD_*.pdf
+            this._coverPDFSearchPattern = coverPDFSearchPattern;        //example *_SPD COVER PAGE.pdf
+        }
+
+
+        //example \\ObinnaPageMerge path
+        public string GetProjectPath()
+        {
+            return this._projectPath;
+        }
+
+
+
+        //example \\XYZ_SPD_01012017.pdf path
+        public string[] GetNonCoverPDFPath()
+        {
+            this._pathA = Directory.GetFiles(this._projectPath ,
+                this._nonCoverPDFSearchPattern);
+
+            return this._pathA;
+        }
+
+
+
+        //example \\XYZ_SPD COVER PAGE.pdf path
+        public string[] GetCoverPDFPath()
+        {
+            this._pathB = Directory.GetFiles(this._projectPath ,
+                this._coverPDFSearchPattern);
+
+            return this._pathB;
+        }
+
+
+
+        //example XYZ_SPD_01012017.pdf
+        public string[] GetNonCoverPDFFileName()
+        {
+            this._nonCoverPDFFileName = Directory.GetFiles(this._projectPath ,
+                this._nonCoverPDFSearchPattern).Select(Path.GetFileName).ToArray();
+
+            return this._nonCoverPDFFileName;
+        }
+
+
+        //example XYZ_SPD COVER PAGE.pdf
+        public string[] GetCoverPDFFileName()
+        {
+            this._coverPDFFileName = Directory.GetFiles(this._projectPath ,
+                this._coverPDFSearchPattern).Select(Path.GetFileName).ToArray();
+
+            return this._coverPDFFileName;
+        }
+
+
+        //example XYZ_SPD COVER PAGE
+        public string GetFileNameWithoutExtension(string fileName)
+        {
+            return fileName.Substring(0 , fileName.IndexOf("_"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using ceTe.DynamicPDF.Merger;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace PCSProject
+{
+    sealed
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();                                 //Track application performace
+            sw.Start();
+
+            MergeDocument document;
+            int value = 1, decimalLength = 0;                               //decimalLength and num are both used for
+                                                                            //creating an incremental string value 
+                                                                            //pattern. An example is 00001, 00002 etc. 
+
+
+            //Create instance of PDFExtract class to access reusable methods
+            PDFTool obj = new PDFTool("PCS_technical_exercise", "*_SPD_*.pdf" , "*_SPD COVER PAGE.pdf");
+
+            //Use Task to improve application performance when locating individual files.
+            Task<string[]> t1 = Task.Factory.StartNew(() => obj.GetNonCoverPDFPath());
+            Task<string[]> t2 = Task.Factory.StartNew(() => obj.GetCoverPDFPath());
+            Task<string[]> t3 = Task.Factory.StartNew(() => obj.GetNonCoverPDFFileName());
+            Task<string[]> t4 = Task.Factory.StartNew(() => obj.GetCoverPDFFileName());
+            Task.WhenAll(t1 , t2 , t3 , t4);
+
+            //Get project directory and PDF file path
+            string path = obj.GetProjectPath();                             //application path
+            string[] nonCoverPDFPath = t1.Result;                           //non-cover pdf filepath
+            string[] coverPDFPath = t2.Result;                              //cover pdf filepath
+
+            //Get PDF file names
+            string[] nonCoverPDFFileName = t3.Result;                       //Containing 'SPD COVER PAGE' in file name
+            string[] coverPDFFileName = t4.Result;                          //Containing only 'SPD' in file name
+
+
+            try
+            {
+                for (int i = 0; i < nonCoverPDFPath.Length; i++)
+                {
+
+                    // Merge the two documents
+                    document = MergeDocument.Merge(nonCoverPDFPath[i] , coverPDFPath[i]);
+
+                    //Get only the filename (without) its file's extension
+                    string fileNameExtract = obj.GetFileNameWithoutExtension(nonCoverPDFFileName[i]);
+
+                    //pad integer value with four leading zeros 
+                    decimalLength = value.ToString("D").Length + 4;
+                    string valuePattern = value.ToString("D" + decimalLength.ToString());
+
+                    //increment value
+                    value++;
+
+                    // Save new PDF file using only fileNameExtract and valuePattern
+                    string newFile = path + @"\" + fileNameExtract + "_" + valuePattern + ".pdf";
+                    document.Draw(newFile);
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("File: " + fileNameExtract + "_" + valuePattern + ".pdf" +
+                        " succsessfully created in ");
+                    Console.WriteLine("Directory: " + path);
+                    Console.WriteLine("\n=========");
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sw.Stop();
+                long time = sw.ElapsedMilliseconds;
+                Console.WriteLine("Execution time (milleseconds): " +time);     //Performance metric report
+                Console.ReadLine();
+            }
+
+            
+
+        }
+    }
+}
